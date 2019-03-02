9a2b0315ea16883afc565bd6e5262b61096c6586
jpclark6 <jpclark6@gmail.com>
2019-03-02 16:00:35 -0700

    functionality and basic styling

--- main.js
+++ main.js
@@ -1,23 +1,56 @@
 var number = 0;
+var guesses = 0;
+var minNumber = 1;
+var maxNumber = 100;
 
 function resetGame(){
-  number = parseInt(Math.random() * 100);
+  number = parseInt(Math.random() * (maxNumber - minNumber) + minNumber);
+  console.log(number);
+  $('.answer').text(number);
+  guesses = 0;
+  $('.last-guess').text('');
+  $('.last-guess-result').text('');
+  $('.reset').prop('disabled', true);
+  $('.guess').val('');
+  $('.clear').prop('disabled', true);
+  $('.min-number').val(minNumber);
+  $('.max-number').val(maxNumber);
+  $('.low').text(minNumber);
+  $('.high').text(maxNumber);
 }
+
+$('.guess').on('keyup click', function(){
+  var currentValue = $('.guess').val();
+  if(currentValue === ''){
+    $('.clear').prop('disabled', true);
+    console.log('disable');
+  } else {
+    $('.clear').prop('disabled', false);
+    console.log('enable');
+  }
+});
 
 $('.submit-guess').on('click', function(e){
   e.preventDefault();
-  var guess = $('.guess').val();
-  if(guess > 100 || guess < 1){
+  var guess = parseInt($('.guess').val());
+  if(guess > maxNumber || guess < minNumber){
     $('.last-guess').text('Number out of range')
-  } else {
+  } else if(guess >= minNumber || guess <= maxNumber){
     $('.last-guess').text(guess)
     checkGuess(guess);
+    guesses += 1;
+    $('.guess').val('');
+    $('.reset').prop('disabled', false);
+    $('.clear').prop('disabled', true);
+  } else {
+    $('.last-guess').text('Invalid guess')
   }
 });
 
 $('.clear').on('click', function(e){
   e.preventDefault();
   $('.guess').val('');
+  $('.clear').prop('disabled', true);
 });
 
 $('.reset').on('click', function(e){
@@ -32,10 +65,19 @@ function checkGuess(guess){
   } else if(guess < number){
     answer = 'That is too low';
   } else {
-    answer = 'BOOM!';
+    answer = 'BOOM! Range has been increased by 20';
+    maxNumber += 10;
+    minNumber -= 10;
   }
   $('.last-guess-result').text(answer);
 }
+
+$('.change-range').on('click', function(e){
+  e.preventDefault();
+  minNumber = parseInt($('.min-number').val());
+  maxNumber = parseInt($('.max-number').val());
+  resetGame();
+})
 
 $(document).ready(function(){
   resetGame();
